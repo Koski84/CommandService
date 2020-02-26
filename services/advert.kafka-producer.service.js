6227be9f174860376e688667f03259cdcb654658
Óscar Lamelas Damas <lamelas84@gmail.com>
2020-02-26 01:20:41 +0100

    Aplico standard

--- services/advert.kafka-producer.service.js
+++ services/advert.kafka-producer.service.js
@@ -1,34 +1,33 @@
 require('dotenv').config('.env')
-const kafka = require('kafka-node');
+const kafka = require('kafka-node')
 
 exports.postAdvert = async function (advert) {
-  const { KAFKA_HOST:host, KAFKA_PORT:port, KAFKA_TOPIC:topic } = process.env
-  let kafka_options = { kafkaHost: `${host}:${port}` };
-  
-  let client = new kafka.KafkaClient(kafka_options);
-  let producer = new kafka.Producer(client);
+  const { KAFKA_HOST: host, KAFKA_PORT: port, KAFKA_TOPIC: topic } = process.env
+  const kafkaOptions = { kafkaHost: `${host}:${port}` }
+
+  const client = new kafka.KafkaClient(kafkaOptions)
+  const producer = new kafka.Producer(client)
 
   console.log(advert)
-  let payloads = [
+  const payloads = [
     {
       topic: topic,
       messages: JSON.stringify(advert)
     }
-  ];
+  ]
 
   producer.send(payloads, (err, data) => {
     if (err) {
-      console.error(err);
-      console.error('[kafkaProducer -> '+ topic +']: broker update failed');
+      console.error(err)
+      console.error('[kafkaProducer -> ' + topic + ']: broker update failed')
     } else {
-      console.log('[kafkaProducer -> '+ topic +']: broker update success');
+      console.log('[kafkaProducer -> ' + topic + ']: broker update success')
     }
-  });
+  })
 
-  producer.on('error', function(err) {
-    console.error(err);
-    console.error('[kafkaProducer -> '+ topic +']: connection errored');
-    throw err;
-  });
+  producer.on('error', function (err) {
+    console.error(err)
+    console.error('[kafkaProducer -> ' + topic + ']: connection errored')
+    throw err
+  })
 }
-
